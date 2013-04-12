1270bfbacf9832223a9375f57d5613c0485a0644
vipul.borikar <vipul.borikar@calsoftinc.com>
2013-04-12 19:07:36 +0530

    Changed the Home Page. Added div element for each quote.

--- mydjango/quotes/views.py
+++ mydjango/quotes/views.py
@@ -5,26 +5,28 @@ from django.http import HttpResponse
 from quotes.models import Quotes
 
 class QuotesForm(forms.Form):
-	quotes = forms.CharField(widget = forms.Textarea(attrs={'size': 20, 'title': 'Your name','cols':70}))
+	quotes = forms.CharField(widget = forms.Textarea(attrs={'size': 20, 'title': 'Your name','cols':70,'value':'Write a Quote'}))
 
 
 
 def index(request):
 	
 	model_obj = Quotes()
+	all_quotes = Quotes.objects.all()
+	
+
 	if request.method == 'POST':
 
 		form = QuotesForm(request.POST)
 		if form.is_valid():
-                        quote_text = form.cleaned_data['quotes']
-                        model_obj.quote = quote_text
-                        model_obj.save()
-			print "OK"
+			quote_text = form.cleaned_data['quotes']
+			model_obj.quote = quote_text
+			model_obj.save()
 
-		return HttpResponse("thanks")
+		#return HttpResponse("thanks")
 	else:
 		form = QuotesForm()
 
 
-	return render(request, 'home_page.html', {'form': form,})
+	return render(request, 'home_page.html', {'form': form,'all_quotes':all_quotes})
 	
